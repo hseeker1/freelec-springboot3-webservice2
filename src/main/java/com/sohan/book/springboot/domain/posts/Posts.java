498362f246cdac057fb118a782ac08e57da92d39
hseeker1 <seungoh.han@gmail.com>
2021-01-05 17:44:10 +0900

    gitignore 등록

--- src/main/java/com/sohan/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/sohan/book/springboot/domain/posts/Posts.java
@@ -1,6 +1,6 @@
 package com.sohan.book.springboot.domain.posts;
 
-import javafx.geometry.Pos;
+import com.sohan.book.springboot.domain.posts.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,14 +14,13 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-
 public class Posts extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
 
-    @Column(length= 500, nullable = false)
+    @Column(length = 500, nullable = false)
     private String title;
 
     @Column(columnDefinition = "TEXT", nullable = false)
@@ -30,17 +29,14 @@ public class Posts extends BaseTimeEntity {
     private String author;
 
     @Builder
-    public Posts(String title, String content, String author){
+    public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
     }
 
-    public void update(String title, String content){
+    public void update(String title, String content) {
         this.title = title;
         this.content = content;
-
     }
-
-
 }
--- src/main/java/com/sohan/book/springboot/web/IndexController.java
+++ src/main/java/com/sohan/book/springboot/web/IndexController.java
@@ -1,4 +1,6 @@
 package com.sohan.book.springboot.web;
+import com.sohan.book.springboot.config.auth.LoginUser;
+import com.sohan.book.springboot.config.auth.dto.SessionUser;
 import com.sohan.book.springboot.service.PostsService;
 import com.sohan.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -6,18 +8,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-
-    public String index(Model model)
+    public String index(Model model, @LoginUser SessionUser user)
     {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
